dd2948dbe8c57edc2cedb2e6fa481a886a730054
vvcaw <eric.elflein@googlemail.com>
2022-11-08 00:50:20 +0100

    Create JFrame wrapper class

---
+++ src/main/java/io/qemfd/pgdp/seam/visualizer/ImagePanel.java
@@ -0,0 +1,10 @@
+package io.qemfd.pgdp.seam.visualizer;
+
+import javax.swing.*;
+
+public class ImagePanel extends JPanel {
+   
+    public ImagePanel(int[] image, int[] mask) {
+
+    }
+}
--- src/main/java/io/qemfd/pgdp/seam/visualizer/Visualizer.java
+++ src/main/java/io/qemfd/pgdp/seam/visualizer/Visualizer.java
@@ -1,4 +1,16 @@
 package io.qemfd.pgdp.seam.visualizer;
 
-public class Visualizer {
+import javax.swing.*;
+
+public class Visualizer extends JFrame {
+
+    public Visualizer(int[] image, int[] mask) {
+        setTitle("Seam Carving");
+        setLocation(0, 0);
+        getContentPane().setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        add(new ImagePanel(image, mask));
+    }
 }

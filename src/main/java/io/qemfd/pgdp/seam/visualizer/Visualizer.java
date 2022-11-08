c4ace494adf560b6d76e28637c89fa5a53c9d170
jonasfeld <29547401+jonasfeld@users.noreply.github.com>
2022-11-08 22:20:40 +0100

    Add next & previous method and re-format code

--- src/main/java/io/qemfd/pgdp/seam/visualizer/Visualizer.java
+++ src/main/java/io/qemfd/pgdp/seam/visualizer/Visualizer.java
@@ -4,13 +4,13 @@ import javax.swing.*;
 
 public class Visualizer extends JFrame {
 
-    public Visualizer(int[] image, int[] mask, int width, int height) {
-        setTitle("Seam Carving");
-        getContentPane().setLayout(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setSize(width, height);
-        setVisible(true);
-        add(new ImagePanel(image, mask, width, height));
-    }
+	public Visualizer(int[] image, int[] mask, int width, int height) {
+		setTitle("Seam Carving");
+		getContentPane().setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setSize(width, height);
+		setVisible(true);
+		add(new ImagePanel(image, mask, width, height));
+	}
 }

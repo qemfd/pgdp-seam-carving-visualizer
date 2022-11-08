7b016dbfc0b62590eb9d36ee8f4a0de245f594c0
vvcaw <eric.elflein@googlemail.com>
2022-11-08 01:19:36 +0100

    Add layout for custom codebase

--- src/main/java/io/qemfd/pgdp/seam/visualizer/SeamCarving.java
+++ src/main/java/io/qemfd/pgdp/seam/visualizer/SeamCarving.java
@@ -1,4 +1,29 @@
 package io.qemfd.pgdp.seam.visualizer;
 
 public class SeamCarving {
+
+    public int computeGradientMagnitude(int v1, int v2) {
+        return 0;
+    }
+
+    public void toGradientMagnitude(int[] picture, int[] gradientMagnitude, int width, int height) {
+    }
+
+    public void combineMagnitudeWithMask(int[] gradientMagnitude, int[] mask, int width, int height) {
+    }
+
+    public void buildSeams(int[][] seams, long[] seamWeights, int[] gradientMagnitude, int width, int height) {
+    }
+
+    public int[] highlightLeastWeightSeam(int[] image, int[] mask, int width, int height) {
+        return new int[0];
+    }
+
+    public void removeSeam(int[] seam, int[] image, int height, int oldWidth) {
+    }
+
+    public int[] shrink(int[] image, int[] mask, int width, int height, int newWidth) {
+        return new int[0];
+    }
 }
+
